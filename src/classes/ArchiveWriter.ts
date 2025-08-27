899a8bc6c02eacfa37e8c5cef0a37e9a2ef82429
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2025-08-28 00:58:52 +0300

    Update

--- src/classes/ArchiveWriter.ts
+++ src/classes/ArchiveWriter.ts
@@ -3,8 +3,8 @@ import { FILE_NAME_LENGTH_BYTES, MAGIC_BYTES } from '../common/constants';
 
 export class ArchiveWriter extends Duplex {
     public archiveSizeBytes = 0;
+    public isBackpressured = false;
     private readonly buffer: Buffer[] = [];
-    private shouldPush = false;
     public addFile(fileName: string) {
         const nameBuffer = Buffer.from(fileName, 'utf-8');
         const nameLengthBuffer = Buffer.alloc(FILE_NAME_LENGTH_BYTES);
@@ -13,41 +13,34 @@ export class ArchiveWriter extends Duplex {
 
         const entryBuffer = Buffer.concat([MAGIC_BYTES, nameLengthBuffer, nameBuffer]);
 
-        this.buffer.push(entryBuffer);
-
-        if (this.shouldPush) {
-            this.shouldPush = this.pushBuffer(this.buffer.shift()!);
-        }
+        this.write(entryBuffer);
     }
     public _write(chunk: any, encoding: BufferEncoding, callback: (error?: Error | null) => void): void {
         this.buffer.push(chunk);
 
-        if (this.shouldPush) {
-            this.shouldPush = this.pushBuffer(this.buffer.shift()!);
-        }
+        this.drain();
 
         callback();
     }
     public _read(size: number) {
-        let buffer: Buffer | undefined;
-        let shouldPush = true;
-
-        while ((buffer = this.buffer.shift()) &&
-            (shouldPush = this.pushBuffer(buffer))) { }
-
-        this.shouldPush = shouldPush;
+        this.drain();
     }
     public _final(callback: (error?: Error | null) => void): void {
-        for (const buffer of this.buffer) {
-            this.pushBuffer(buffer);
-        }
+        this.drain();
 
         this.push(null);
 
         callback();
     }
+    private drain() {
+        let buffer: Buffer | undefined;
+
+        while ((buffer = this.buffer.shift())) {
+            this.pushBuffer(buffer);
+        }
+    }
     private pushBuffer(buffer: Buffer) {
         this.archiveSizeBytes += buffer.byteLength;
-        return this.push(buffer);
+        this.isBackpressured = this.push(buffer) === false;
     }
 }
